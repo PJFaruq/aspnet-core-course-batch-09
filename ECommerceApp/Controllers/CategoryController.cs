56ad078a651b11fdb5a1a8560abe3e4ac3d4c92d
Omar Faruq <omar.faruq@brainstation-23.com>
2026-01-21 00:08:52 +0600

    Implement category create functionality

---
+++ ECommerceApp/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using ECommerceApp.Data;
+using ECommerceApp.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ECommerceDbContext _context;
+
+        public CategoryController(ECommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET: Category/Create
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Description")] Category category)
+        {
+            category.CreatedDate = DateTime.Now;
+
+            //Server side validation
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            Category category1 = _context.Categories.FirstOrDefault(m => m.Name == category.Name);
+
+            if (category1 == null)
+            {
+                _context.Categories.Add(category);
+                await _context.SaveChangesAsync();
+            }
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ECommerceApp/Domain/Entities/Category.cs
+++ ECommerceApp/Domain/Entities/Category.cs
@@ -4,11 +4,19 @@ namespace ECommerceApp.Domain.Entities
 {
     public class Category
     {
+
         public int Id { get; set; }  //Primary key
-        [Required]
-        [StringLength(50)]
+
+        [Required(ErrorMessage = "Category name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 to 50 character")]
+        [Display(Name = "Category Name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description name is required")]
+        [StringLength(50, ErrorMessage = "Name must be up to 500 characters")]
+        [Display(Name = "Category Description")]
         public string? Description { get; set; }
+
         public DateTime CreatedDate { get; set; }
 
         //Relationship: One category can have many Products

f6bb6d9a22cdbbb494cf8d33b50cfb28d197937b
Omar Faruq <omar.faruq@brainstation-23.com>
2026-03-28 09:24:03 +0600

    implement cookie based authentication

---
+++ CookieAuthDemo/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CookieAuthDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            if (username == "admin" && password == "123")
+            {
+
+                //Generating Security Context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,username),
+                    new Claim(ClaimTypes.Role,"Customer"),
+
+                    //Custom Claims
+                    new Claim("Department","HR"),
+                };
+
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+
+                var principal = new ClaimsPrincipal(identity);
+
+                //Setting security context into cookie
+                await HttpContext.SignInAsync("MyCookieAuth", principal);
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Logout(string username, string password)
+        {
+            await HttpContext.SignOutAsync("MyCookieAuth");
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ CookieAuthDemo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using CookieAuthDemo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CookieAuthDemo.Controllers
+{
+    // [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdminPage()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
